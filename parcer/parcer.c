0a0d6d63d0f5401563bfa9ae01e0dffefb5bad0a
Minsu Kim <minsuki2@c5r1s2.42seoul.kr>
2022-08-24 10:54:29 +0900

    feat : make parcer branch #10

--- parcer/parcer.c
+++ parcer/parcer.c
@@ -6,11 +6,12 @@
 /*   By: minsuki2 <minsuki2@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 10:31:09 by minsuki2          #+#    #+#             */
-/*   Updated: 2022/08/24 10:34:11 by minsuki2         ###   ########.fr       */
+/*   Updated: 2022/08/24 10:53:51 by minsuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parcer.h"
+#include "../lexer/lexer.h"
 #include "../minishell.h"
 // #include "minishell.h"
 
@@ -19,11 +20,22 @@
 void recursive_tree()
 {
 
+
 }
 
+int parcer(t_lx_token *head)
+{
+	t_tree		*point;
+	t_lx_token	*cur_node;
+
+	cur_node = head;
+	while (cur_node)
+	{
+		is_metachracter();
+		is_
 
 
-int parcer()
-
-
+		cur_node = cur_node->next;
+	}
+}
 
