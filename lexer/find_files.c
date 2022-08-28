704e16e29c79ad5c9cbc27b358152dd55c57a46a
minsuki2 <minsuki2@student.42seoul.kr>
2022-08-28 10:02:58 +0900

    feat : files matching#17

--- lexer/find_files.c
+++ lexer/find_files.c
@@ -6,7 +6,7 @@
 /*   By: minsuki2 <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 23:35:33 by minsuki2          #+#    #+#             */
-/*   Updated: 2022/08/27 23:35:50 by minsuki2         ###   ########.fr       */
+/*   Updated: 2022/08/28 09:56:01 by minsuki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ int is_matching_file(char *input, t_file  * const files, int dir_flag, int wildc
 
 	i = 0;
 	cnt = 0;
-	ft_strlen(input);
+	// ft_strlen(input);
 	while (i < files->n)
 	{
 		if (dir_flag && files[i].type != DT_DIR)
 			continue ;
 		files[i].match_flag = check_match(input, files[i].name);
 		if (files[i].match_flag && ++cnt)
-			if (wildcard_flag)
+			if (!wildcard_flag)
 				return (cnt);
 		i++;
 	}
@@ -40,27 +40,25 @@ int main(int ac, char *av[])
 {
 	t_file	*files;
 
-	if (ac != 3)
-		return (0);
-	files = check_cur_dir(av[1]);
+	files = check_cur_dir(getenv("PWD"));
 	for (int i = 0; i < files->n; i++)
 	{
-		printf("%d\n", files[i].n);
-		printf("%s\n", files[i].name);
-		printf("%d\n", files[i].match_flag);
-		printf("%d\n", files[i].type);
-		printf("%s\n", files[i].pwd);
+		// printf("%d\n", files[i].n);
+		printf("name\t:\t%s\n", files[i].name);
+		printf("mflag\t:\t%d\n", files[i].match_flag);
+		printf("type\t:\t%d\n", files[i].type);
+		// printf("%s\n", files[i].pwd);
 		printf("\n");
 	}
-	if (is_matching_file(av[2], files) == 0)
-		return (0);
+	is_matching_file("*.c", files, 0, 1);
+	printf("------------------------------------\n");
 	for (int i = 0; i < files->n; i++)
 	{
-		printf("%d\n", files[i].n);
-		printf("%s\n", files[i].name);
-		printf("%d\n", files[i].match_flag);
-		printf("%d\n", files[i].type);
-		printf("%s\n", files[i].pwd);
+		// printf("%d\n", files[i].n);
+		printf("name\t:\t%s\n", files[i].name);
+		printf("mflag\t:\t%d\n", files[i].match_flag);
+		printf("type\t:\t%d\n", files[i].type);
+		// printf("%s\n", files[i].pwd);
 		printf("\n");
 	}
 	return (0);
