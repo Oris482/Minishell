baf5da3e80b99dba775e1955f784be2840e3a561
jaesjeon <jaesjeon@student.42seoul.kr>
2022-09-07 03:08:10 +0900

    fixed: syntax error case - < a (ls | ls) #10

--- parser/check_syntax_error.c
+++ parser/check_syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 15:24:20 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/09/04 20:11:34 by minsuki2         ###   ########.fr       */
+/*   Updated: 2022/09/07 03:00:52 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,12 +47,22 @@ static int	_check_parentheses_syntax(t_lx_token **token, \
 										int *parentheses_counter)
 {
 	const t_lx_token	*prev_token = (*token)->prev;
+	t_lx_token			*find_token;
 
 	if ((*token)->token_type == PARENTHESES_OPEN)
 	{
 		(*parentheses_counter)++;
 		if (!prev_token || prev_token->token_type != WORD)
 			return (SUCCESS);
+		else if (prev_token && prev_token->prev && prev_token->prev->next != NULL)
+		{
+			find_token = (t_lx_token *)prev_token;
+			while (find_token->token_str == NULL)
+				find_token = find_token->prev;
+			if (find_token->token_type == WORD && find_token->prev->next != NULL && \
+				is_redi_token(find_token->prev))
+				return (SUCCESS)
+;		}
 	}
 	else
 	{
@@ -86,14 +96,16 @@ unsigned int	check_syntax_error(t_lx_token *cur_node)
 	const int	head_token_type = cur_node->token_type;
 	int			parentheses_counter;
 
+	parentheses_counter = 0;
 	if (head_token_type == PIPE || head_token_type == AND_IF \
 		|| head_token_type == OR_IF || head_token_type == PARENTHESES_CLOSE)
 	{
 		print_error_syntax(get_token_str(cur_node));
 		return (SYNTAX_ERROR_EXIT_CODE);
 	}
-	cur_node = cur_node->next;
-	parentheses_counter = 0;
+	if (head_token_type == PARENTHESES_OPEN)
+		parentheses_counter++;
+	// cur_node = cur_node->next;
 	while (cur_node)
 	{
 		if (cur_node->token_type != WORD && _check_syntax_middleware(&cur_node, \
