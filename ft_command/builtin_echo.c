e9669ad57541b042c566509273f826d0cf4b7e19
jaesjeon <jaesjeon@student.42seoul.kr>
2022-09-18 05:07:59 +0900

    fixed: exit code, echo, cd #44

--- ft_command/builtin_echo.c
+++ ft_command/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 22:19:28 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/09/17 22:49:56 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2022/09/18 04:50:00 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,39 @@
 #include "ft_token.h"
 #include "ft_print.h"
 
+void	handle_echo_option(t_lx_token **token, int *option)
+{
+	char	*str;
+	int		idx;
+
+	while (*token)
+	{
+		idx = 2;
+		str = get_token_str(*token);
+		if (str && str[0] && str[0] == '-' && str[1] && str[1] == 'n')
+		{
+			while (str[idx] && str[idx] == 'n')
+				idx++;
+			if (str[idx] == '\0')
+			{
+				*token = (*token)->next;
+				*option |= TRUE;
+			}
+			else
+				break ;
+		}
+		else
+			break ;
+	}
+}
+
 int	builtin_echo(t_lx_token *token)
 {
 	int	no_newline_option;
 
 	no_newline_option = FALSE;
 	token = token->next;
-	if (token && ft_strcmp("-n", get_token_str(token)) == SUCCESS)
-	{
-		no_newline_option = TRUE;
-		token = token->next;
-	}
+	handle_echo_option(&token, &no_newline_option);
 	while (token)
 	{
 		ft_putstr_fd(get_token_str(token), STDOUT_FILENO);
